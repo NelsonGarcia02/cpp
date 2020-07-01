51493f4f0c730a79b77435bd456c0e9d7ba2bb24
NelsonGarcia02 <garcianelson0207@gmail.com>
2020-07-01 13:08:13 -0600

    Ejercicios

---
+++ Ejercicios/E2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std:
+int main(int argc, char** argv){
+    string nombre;
+    cout<<"Ingrese su nombre:";
+    cin>>nombre;
+    cout<<"Su nombre es:"<<nombre;
+    
+    return 0;
+}
